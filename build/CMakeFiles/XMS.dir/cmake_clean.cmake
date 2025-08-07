e6cfbdbd87301d11c058e221e5b6a3bd1be3e8d8
phenom64 <founder.superk@hotmail.com>
2025-08-08 02:19:01 +0700

    feat: conversion to Vulkan from OpenGL, conversion of shaders from GLSL to SPIR-V

---
+++ build/CMakeFiles/XMS.dir/cmake_clean.cmake
@@ -0,0 +1,18 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/XMS_autogen.dir/AutogenUsed.txt"
+  "CMakeFiles/XMS_autogen.dir/ParseCache.txt"
+  "XMS_autogen"
+  "CMakeFiles/XMS.dir/XMS_autogen/mocs_compilation.cpp.o"
+  "CMakeFiles/XMS.dir/XMS_autogen/mocs_compilation.cpp.o.d"
+  "CMakeFiles/XMS.dir/src/main.cpp.o"
+  "CMakeFiles/XMS.dir/src/main.cpp.o.d"
+  "XMS"
+  "XMS.pdb"
+  "XMS_autogen/mocs_compilation.cpp"
+  "XMS_autogen/timestamp"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/XMS.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
